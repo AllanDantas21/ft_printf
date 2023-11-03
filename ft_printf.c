9bc7357ca3cc99ffe512df71b857429056d1888f
AllanDantas21 <allandantas21.ad@gmail.com>
2023-11-02 22:28:54 -0300

    printf_42

---
+++ ft_printf.c
@@ -0,0 +1,48 @@
+#include "libft.h"
+
+static	print_fmt(char arg, va_list ap)
+{
+ 	int	count;
+
+	count = 0;
+	if (arg == 'c')
+		print_char(va_arg(ap, int));
+	if (arg == 'd')
+		print_integer(va_arg(ap, int));
+	if (arg == 's')
+		print_string(va_arg(ap, char *));
+	if (arg == 'p')
+		;
+	if (arg == 'i')
+		;
+	if (arg == 'u')
+		;
+	if (arg == 'x')
+		;
+	if (arg == 'X')
+		;
+	if (arg == '%')
+		;
+}
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int	count;
+
+	va_start(ap, format);
+	count = 0;
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			count += print_format(*format, ap);
+		}
+		else
+			count += write(1, format, 1);
+		format++;
+	}
+	va_end(ap);
+	return (count);
+}
