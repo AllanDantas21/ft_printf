9d4cc65e0cef8b3bdaee24a00c5ba219f52f44a8
AllanDantas21 <allandantas21.ad@gmail.com>
2023-11-03 00:30:28 -0300

    att

--- main.c
+++ main.c
@@ -2,11 +2,9 @@
 
 int	main(void)
 {	
-	int	i = 42;
-
 	ft_printf("abacate\n");
 	ft_printf("printando string: %s\n", "abacate");
-	ft_printf("printando decimais: %d\n", i);
+	ft_printf("printando decimais: %d, %d\n", 42, -42);
 	ft_printf("printando char: %c %c\n", 'a', 'b');
 	ft_printf("printando percento: %%\n");
 }
