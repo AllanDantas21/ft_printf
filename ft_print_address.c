d8becf8aed41300509ba43162a907747eebe676f
AllanDantas21 <allandantas21.ad@gmail.com>
2023-11-03 23:37:57 -0300

    add address handler

---
+++ ft_print_address.c
@@ -0,0 +1,13 @@
+#include "printf.h"
+
+int	print_address(unsigned long p)
+{
+	int	counter;
+	char	*str;
+	
+	counter = print_string("0x");
+	str = itoa_base(p, 16, "0123456789abcdef");
+	counter += print_string(str);
+	free(str);
+	return (counter);
+}
