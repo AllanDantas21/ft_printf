6893c29254d4c47c45c88e8b5d1aaafe053540fe
AllanDantas21 <allandantas21.ad@gmail.com>
2023-11-05 00:36:09 -0300

    back to old itoa

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -1,32 +1,6 @@
 #include "ft_printf.h"
 
-static long	handle_negative(long *value)
-{
-    if (*value < 0)
-    {
-                *value = -*value;
-		return (1);
-    }
-    return (0);
-}
-
-static void	fill_itoa(int nb, char *str, int base, char *chr, int len)
-{
-	str[len] = '\0';
-       	while (len--)
-        {
-                str[len] = chr[(nb % base)];
-                nb /= base;
-        }
-}
-
-static void	add_sign(char *str, int sign)
-{
-	if (sign == 1)
-		str[0] = '-';
-}
-
-char	*itoa_base(int n, int base, char *chr)
+char	*itoa_base(int n, int base, char* chr)
 {
 	char	*str;
 	long	nb;
@@ -34,12 +8,23 @@ char	*itoa_base(int n, int base, char *chr)
 	int		len;
 
 	nb = n;
+	sign = 0;
 	len = sizenum_base(nb, base);
-	sign = handle_negative(&nb);
+	if (nb < 0)
+	{
+		sign = 1;
+		nb = -nb;
+	}
 	str = (char *)malloc(sizeof(char) * (len + 1));
 	if (!str)
 		return (NULL);
-	fill_itoa(nb, str, base, chr, len);
-	add_sign(str, sign);
+	str[len] = '\0';
+	while (len--)
+	{
+		str[len] = chr[(nb % base)];
+		nb /= base;
+	}
+	if (sign == 1)
+		str[0] = '-';
 	return (str);
 }
