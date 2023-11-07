08a3089df8ea9bd7a0403d6f10a8f0f16c160eb5
AllanDantas21 <allandantas21.ad@gmail.com>
2023-11-06 23:33:23 -0300

    move sizenum

--- ft_itoa_base.c
+++ ft_itoa_base.c
@@ -1,5 +1,25 @@
 #include "ft_printf.h"
 
+static int     sizenum_base(long n, int base)
+{
+	int     count;
+
+	count = 0;
+	if (n < 0)
+	{
+		n = -n;
+		count++;
+	}
+	else if (n == 0)
+		return (1);
+	while (n > 0)
+	{
+		count++;
+		n = n / base;
+	}
+	return (count);
+}
+
 static int	check_negative(long *value)
 {
 	if (*value < 0)
--- ft_printf.h
+++ ft_printf.h
@@ -9,7 +9,6 @@ int	print_decimal(int decimal);
 int	print_char(char c);
 int	print_string(char *str);
 int	print_percent(void);
-int	sizenum_base(long n, int base);
 int	print_hexa(unsigned int n);
 int	print_hexa_upper(unsigned int n);
 int	print_unsigned(unsigned long n);
